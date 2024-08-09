bc23df86171c5f12478e1500c8ca8452832f3c41
saad <saad@globex>
2024-08-09 17:37:20 +0400

    ddd

---
+++ src/components/Team.jsx
@@ -0,0 +1,133 @@
+import React from "react";
+
+export const PrivacyPolicy = () => {
+  return (
+    <div id="privacy-policy" className="text-center">
+      <div className="container">
+        <div className="col-md-8 col-md-offset-2 section-title">
+          <h2>Privacy Policy</h2>
+          <p>
+            These terms and conditions outline the rules and regulations for the
+            use of [YOUR NEW WEBSITE NAME], located at [YOUR WEBSITE URL]. By
+            accessing this website, we assume you accept these terms and
+            conditions. Do not continue to use [YOUR WEBSITE NAME] if you do not
+            agree to all of the terms and conditions stated on this page.
+          </p>
+        </div>
+        <div id="row">
+          <div className="col-md-12">
+            <div className="thumbnail">
+              <div className="caption">
+                <h4>Disclaimer</h4>
+                <p>
+                  The Client understands and agrees that all investment and/or
+                  trading decisions are based on the Client's own investment
+                  objectives and evaluation of prevailing financial
+                  circumstances. [Your New Website Name] is merely a facilitator
+                  providing access to its Clients to participate in the
+                  financial and commodity markets and does not seek to influence
+                  or otherwise affect the decision-making process of its Clients
+                  in any way whatsoever.
+                </p>
+
+                <h4>Account Opening and Registration</h4>
+                <p>
+                  Before availing of any online trading services on the
+                  Platform, the Client is required to complete the registration
+                  process as may be prescribed from time to time. To register
+                  yourself, you are required to follow the procedure mentioned
+                  on the Platform, along with any instructions communicated to
+                  you by [Your New Website Name]'s employees or
+                  representatives.
+                </p>
+
+                <h4>Price Data</h4>
+                <p>
+                  [Your New Website Name] does not share real-time price data,
+                  except with third parties where it is required for the orderly
+                  functioning of the securities market, or where permitted by
+                  Applicable Law.
+                </p>
+
+                <h4>Fees and Brokerage</h4>
+                <p>
+                  The Client agrees to pay [Your New Website Name] all
+                  brokerages, transaction charges, commissions, fees, GST, and
+                  other taxes or levies, by whatever name so called, as informed
+                  to the Client by [Your New Website Name] from time to time in
+                  writing.
+                </p>
+
+                <h4>Termination</h4>
+                <p>
+                  [Your New Website Name] may terminate or suspend your account
+                  at any time for any reason, without prior notice. You may also
+                  terminate your account at any time by following the
+                  instructions on the website.
+                </p>
+
+                <h4>Limitation of Liability</h4>
+                <p>
+                  [Your New Website Name] is not liable for any direct or
+                  indirect damages, including but not limited to loss of
+                  capital, data, or use, arising from your use of [Your New
+                  Website Name] or its services.
+                </p>
+
+                <h4>Intellectual Property</h4>
+                <p>
+                  All intellectual property, including in relation to the
+                  Platforms, belongs exclusively to [Your New Website Name] or
+                  is licensed to [Your New Website Name].
+                </p>
+
+                <h4>Indemnities</h4>
+                <p>
+                  The Client agrees to indemnify and hold [Your New Website
+                  Name], its affiliates, partners, directors, employees, and
+                  agents harmless against any losses that may be suffered by it,
+                  its other customers, or any third parties.
+                </p>
+
+                <h4>Force Majeure</h4>
+                <p>
+                  [Your New Website Name] shall not be responsible for any
+                  delays or defaults in the performance of its obligations due
+                  to any contingencies beyond its control.
+                </p>
+
+                <h4>Severability</h4>
+                <p>
+                  In the event that any one or more of the provisions contained
+                  in these Terms become invalid, illegal, or unenforceable, the
+                  validity, legality, and enforceability of the remaining
+                  provisions shall not be in any way affected or impaired.
+                </p>
+
+                <h4>Refund & Cancellation Policy</h4>
+                <p>
+                  The refund & cancellation policy for all payments made or fees
+                  paid towards account opening or availing of any other services
+                  from [Your New Website Name] shall be as follows:...
+                </p>
+
+                <h4>Governing Law, Jurisdiction and Dispute Resolution</h4>
+                <p>
+                  The Terms and the use of the website are governed by and
+                  construed in accordance with the laws of [Your Country].
+                </p>
+
+                <h4>Notice</h4>
+                <p>
+                  All notices, correspondences, or communications issued under
+                  these Terms shall be served in any one or more of the
+                  following modes of communications:...
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
