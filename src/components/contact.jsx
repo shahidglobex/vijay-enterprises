e30c7cb7475abdc03b99e56b13b2f014ebb5e1c8
saad <saad@globex>
2024-08-19 16:56:13 +0500

    address removed

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -99,15 +99,7 @@ export const Contact = (props) => {
             </div>
           </div>
           <div className="col-md-3 col-md-offset-1 contact-info">
-            <div className="contact-item">
-              <h3>Contact Info</h3>
-              <p>
-                <span>
-                  <i className="fa fa-map-marker"></i> Address
-                </span>
-                {props.data ? props.data.address : "loading"}
-              </p>
-            </div>
+           
             <div className="contact-item">
               <p>
                 <span>
